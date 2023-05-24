8ffc2c0aeab3f0ce9502aed4cd89d23128b2b5dc
Kamolov Amir <kamolov.amir2000@yandex.ru>
2023-05-25 00:20:54 +0300

    [fix] register project

--- MVCS/MVCS.Presentation.gRPC/Program.cs
+++ MVCS/MVCS.Presentation.gRPC/Program.cs
@@ -104,6 +104,7 @@ app.UseAuthorization();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<AuthService>();
 app.MapGrpcService<ProjectsService>();
+app.MapGrpcService<ProjectService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 
